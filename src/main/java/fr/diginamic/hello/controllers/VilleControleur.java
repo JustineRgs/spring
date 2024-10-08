27605291f0d422c63ef55a94e2c315f36632e662
justinergs <justineragues@gmail.com>
2024-10-08 15:34:08 +0200

    TP3

--- src/main/java/fr/diginamic/hello/controllers/VilleControleur.java
+++ src/main/java/fr/diginamic/hello/controllers/VilleControleur.java
@@ -1,20 +1,35 @@
 package fr.diginamic.hello.controllers;
 
 import fr.diginamic.hello.model.Ville;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.Arrays;
+import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 public class VilleControleur {
+
+    private List<Ville> villes = new ArrayList<>(List.of(
+            new Ville("Paris", 2148000),
+            new Ville("Marseille", 861635),
+            new Ville("Lyon", 513275)
+    ));
+
     @GetMapping("/villes")
     public List<Ville> getVilles() {
-        return Arrays.asList(
-                new Ville("Paris", 2148000),
-                new Ville("Marseille", 861635),
-                new Ville("Lyon", 513275)
-        );
+        return villes;
+    }
+
+    @PostMapping("/villes")
+    public ResponseEntity<String> ajouterVille(@RequestBody Ville nouvelleVille) {
+        for (Ville ville : villes) {
+            if (ville.getNom().equalsIgnoreCase(nouvelleVille.getNom())) {
+                return new ResponseEntity<>("La ville existe déjà", HttpStatus.BAD_REQUEST);
+            }
+        }
+        villes.add(nouvelleVille);
+        return new ResponseEntity<>("Ville insérée avec succès", HttpStatus.OK);
     }
 }
