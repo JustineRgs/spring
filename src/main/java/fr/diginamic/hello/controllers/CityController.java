08fcf0869044e6f6af0191f0688fd970b06e7b7a
Justine RAGUES <justineragues@gmail.com>
2024-10-15 16:56:39 +0200

    final

--- src/main/java/fr/diginamic/hello/controllers/CityController.java
+++ src/main/java/fr/diginamic/hello/controllers/CityController.java
@@ -14,6 +14,7 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.HttpHeaders;
 
@@ -239,4 +240,7 @@ public class CityController {
         JSONObject jsonObject = new JSONObject(response.toString());
         return jsonObject.getString("nom");
     }
+
+
+
 }
---
+++ src/main/java/fr/diginamic/hello/viewController/CityViewController.java
@@ -0,0 +1,34 @@
+package fr.diginamic.hello.viewController;
+
+import fr.diginamic.hello.model.City;
+import fr.diginamic.hello.services.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+//http://localhost:8080/cities
+@Controller
+public class CityViewController {
+
+    @Autowired
+    private CityService cityService;
+
+    @GetMapping("/cities")
+    public String getIndex(Model model) {
+        model.addAttribute("title", "Cities");
+        model.addAttribute("cities", cityService.findAll());
+        return "city";
+    }
+
+    @GetMapping("/cities/{id}")
+    public String getCity(
+            Model model,
+            @PathVariable long id) {
+        City city = cityService.findById(id);
+        model.addAttribute("title", city.getName());
+        model.addAttribute("city", city);
+        return "cities";
+    }
+}
